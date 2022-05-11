348b2f3ba71f393fdd2d196a2edb91cf0e45deeb
Woutderoy <woutrvz@gmail.com>
2022-05-11 14:03:15 +0200

    V0.3.1 Address verwijderd want is niet nodig als object en aangepast in user entity naar String

--- src/main/java/io/swagger/api/model/Role.java
+++ src/main/java/io/swagger/api/model/Role.java
@@ -1,10 +1,12 @@
-package io.swagger.api.model.DTO;
+package io.swagger.api.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ROLE_ADMIN,
-    ROLE_USER;
+    ROLE_USER,
+    ROLE_EMPLOYEE;
+
+    @Override
     public String getAuthority() {
         return name();
     }
